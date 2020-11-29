bd02eb3360de4cf360d0e5d4c1ea2068cf4df2f6
Andrew <andrew.usik.samogon@gmail.com>
2020-11-30 01:51:18 +0200

    first commit

---
+++ Fibonachi_test/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input length");
+            GetNthFibonacci_Ite(Convert.ToInt16(Console.ReadLine()));
+            Console.ReadLine();
+        }
+        public static void GetNthFibonacci_Ite(int len)
+        {
+            int a = 0, b = 1, c = 0;
+            Console.Write("{0} {1}", a, b);
+            for (int i = 2; i < len; i++)
+            {
+                c = a + b;
+                Console.Write(" {0}", c);
+                a = b;
+                b = c;
+            }
+        }
+    }
+}
